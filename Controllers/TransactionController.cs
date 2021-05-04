b073c55b82396668bf2fafe53dd53caa9fd409cc
patrikns <patrik.n.samuelsson@gmail.com>
2021-05-04 11:30:31 +0200

    Added som of the functionality needed for transferring money.

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Uppgift2BankApp.Models;
+using Uppgift2BankApp.ViewModels;
+
+namespace Uppgift2BankApp.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly BankAppDataContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public TransactionController(BankAppDataContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public IActionResult New(int id)
+        {
+            var viewModel = new TransactionNewViewModel
+            {
+                AccountId = id, Date = DateTime.Now, OperationItems = GetOperations()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult New(TransactionNewViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+
+        private List<SelectListItem> GetOperations()
+        {
+            var l = new List<SelectListItem>
+            {
+                new("Credit", "0"),
+                new("Credit in cash", "1"),
+                new("Credit card withdrawal", "2"),
+                new ("Withdrawal in cash", "3"),
+                new("Remittance to another bank", "4")
+            };
+            return l;
+        }
+    }
+}
---
+++ ViewModels/TransactionNewViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Uppgift2BankApp.Models;
+
+namespace Uppgift2BankApp.ViewModels
+{
+    public class TransactionNewViewModel
+    {
+        public int SelectedOperation { get; set; }
+        public List<SelectListItem> OperationItems { get; set; }
+
+        public int TransactionId { get; set; }
+        public int AccountId { get; set; }
+        public DateTime Date { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Type { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Operation { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Balance { get; set; }
+        [StringLength(50)]
+        public string Symbol { get; set; }
+        [StringLength(50)]
+        public string Bank { get; set; }
+        [StringLength(50)]
+        public string Account { get; set; }
+    }
+}
